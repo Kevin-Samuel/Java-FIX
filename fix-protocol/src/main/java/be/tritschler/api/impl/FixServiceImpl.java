e82a7ca11636081e50bb6296026919d9a634066c
tritschler <marc@tritschler.be>
2013-09-24 10:59:45 +0200

    Create FixServiceImpl.java

---
+++ fix-protocol/src/main/java/be/tritschler/api/impl/FixServiceImpl.java
@@ -0,0 +1,17 @@
+package be.tritschler.fix.api.impl;
+
+import be.tritschler.fix.api.FixService;
+
+public class FixServiceImpl implements FixService {
+
+	public void startSession() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void endSession() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
