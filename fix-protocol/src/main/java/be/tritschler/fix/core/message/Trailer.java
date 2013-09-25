fecaf6f2d4a292d36310fa7caa53b6933b668f89
TRITSCHLER <marc@tritschler.be>
2013-09-25 14:11:18 +0200

    Create Trailer.java

---
+++ fix-protocol/src/main/java/be/tritschler/fix/core/message/Trailer.java
@@ -0,0 +1,19 @@
+package be.tritschler.fix.core.message;
+
+import be.tritschler.fix.core.tags.CheckSum;
+import be.tritschler.fix.core.tags.Tag;
+
+/**
+ * FIX 4.x Trailer, made of tags 93, 89 and 10
+ * 
+ * @author Marc Tritschler
+ *
+ */
+public class Trailer {
+	// 93, 89, 10
+	
+//	private 
+	private CheckSum checkSum;
+	
+	//
+}
