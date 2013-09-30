6be369101bc42d4f1bb4514de1720734a25dc80c
TRITSCHLER <marc@tritschler.be>
2013-09-30 17:02:45 +0200

    Update LogonProcessor.java

--- fix-protocol/src/main/java/be/tritschler/fix/core/message/processor/LogonProcessor.java
+++ fix-protocol/src/main/java/be/tritschler/fix/core/message/processor/LogonProcessor.java
@@ -1,6 +1,7 @@
 package be.tritschler.fix.core.message.processors;
 
 import be.tritschler.fix.core.message.Logon;
+import be.tritschler.fix.core.message.Message;
 
 /**
  * LogonProcessor implements the business logic of a Logon message received.
@@ -10,14 +11,20 @@ import be.tritschler.fix.core.message.Logon;
  */
 public class LogonProcessor implements MessageProcessor<Logon>{
 
+	private Message message;
+	
+	public LogonProcessor(Message message) {
+		this.message = message;
+	}
+	
 	@Override
-	public void validate(Logon message) {
+	public void validate() {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void process(Logon message) {
+	public void process() {
 		// TODO Auto-generated method stub
 		// validate
 		
