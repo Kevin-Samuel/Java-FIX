fc61d6569f8a7eb544b287af57e2af8ee41d3bcb
TRITSCHLER <marc@tritschler.be>
2013-10-02 15:08:11 +0200

    Update FixService.java

--- fix-protocol/src/main/java/be/tritschler/fix/api/FixService.java
+++ fix-protocol/src/main/java/be/tritschler/fix/api/FixService.java
@@ -1,8 +1,14 @@
 package be.tritschler.fix.api;
 
+import java.util.Date;
+
 public interface FixService {
 
+	// session
 	void startSession();
 	void endSession();
 	
+	// trade
+	void buy(String instrument, long maxPrice, long quantity, Date validity);
+	void sell();
 }
