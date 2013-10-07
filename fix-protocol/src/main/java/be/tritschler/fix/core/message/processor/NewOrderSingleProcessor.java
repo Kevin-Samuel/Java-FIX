167789ea3adad9fe810bd197388c220808a6854c
TRITSCHLER <marc@tritschler.be>
2013-10-07 16:47:31 +0200

    Create NewOrderSingleProcessor.java

---
+++ fix-protocol/src/main/java/be/tritschler/fix/core/message/processor/NewOrderSingleProcessor.java
@@ -0,0 +1,18 @@
+package be.tritschler.fix.core.message.processors;
+
+import be.tritschler.fix.core.message.NewOrderSingle;
+
+public class NewOrderSingleProcessor implements MessageProcessor<NewOrderSingle> {
+
+	@Override
+	public void validate() {
+		
+	}
+
+	@Override
+	public void process() {
+
+		
+	}
+
+}
