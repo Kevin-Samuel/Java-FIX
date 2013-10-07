e1b25e5e730dc2e5db1d3021df5e820136e49d8f
TRITSCHLER <marc@tritschler.be>
2013-10-07 16:35:17 +0200

    Update HeartBeat.java

--- fix-protocol/src/main/java/be/tritschler/fix/core/message/HeartBeat.java
+++ fix-protocol/src/main/java/be/tritschler/fix/core/message/HeartBeat.java
@@ -1,5 +1,11 @@
 package be.tritschler.fix.core.message;
 
+import be.tritschler.fix.core.tags.MsgType;
+
 public class HeartBeat {
 
+	
+	public static final String TYPE = MsgType.TYPE_HEARTBEAT;
+	
+	
 }
