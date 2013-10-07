e44e5797fe35dab77f37747965f2a315a9084ed1
TRITSCHLER <marc@tritschler.be>
2013-10-07 17:36:01 +0200

    Update NewOrderSingleProcessor.java

--- fix-protocol/src/main/java/be/tritschler/fix/core/message/processor/NewOrderSingleProcessor.java
+++ fix-protocol/src/main/java/be/tritschler/fix/core/message/processor/NewOrderSingleProcessor.java
@@ -1,18 +1,33 @@
 package be.tritschler.fix.core.message.processors;
 
+import org.apache.log4j.Logger;
+
+import be.tritschler.fix.core.message.Message;
 import be.tritschler.fix.core.message.NewOrderSingle;
 
 public class NewOrderSingleProcessor implements MessageProcessor<NewOrderSingle> {
-
+	
+	private static final Logger logger = Logger.getLogger(NewOrderSingleProcessor.class);
+	
+	private Message message;
+	
+	public NewOrderSingleProcessor(Message message) {
+		this.message = message;
+	}
+	
 	@Override
 	public void validate() {
+		// sysntax
+		
+		// business
 		
 	}
 
 	@Override
 	public void process() {
 
-		
+		validate();
+		logger.debug("processing " + message);
 	}
 
 }
