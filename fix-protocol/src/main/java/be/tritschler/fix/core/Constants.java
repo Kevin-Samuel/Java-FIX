fe6592b7ec5cce1db5399e41a1f91c16a0679c95
TRITSCHLER <tritschler@users.noreply.github.com>
2013-10-10 12:12:12 +0200

    Create Constants.java

---
+++ fix-protocol/src/main/java/be/tritschler/fix/core/Constants.java
@@ -0,0 +1,14 @@
+package be.tritschler.fix.core;
+
+// Although not recommended, I prefer to keep it simple and put all constants here.
+
+public class Constants {
+	
+	public static final char SOH = 1;
+	
+	public static final String EQUAL = "=";
+	
+	// FIX versions
+//	public static final String PROTOCOL = "FIX";	
+//	public static final String FIX40 = "4.0";
+}
