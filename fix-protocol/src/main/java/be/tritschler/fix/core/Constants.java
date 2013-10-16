53d06d91befb18cfe2c25d00977133d6c2cb291c
TRITSCHLER <tritschler@users.noreply.github.com>
2013-10-16 14:51:26 +0200

    Update Constants.java

--- fix-protocol/src/main/java/be/tritschler/fix/core/Constants.java
+++ fix-protocol/src/main/java/be/tritschler/fix/core/Constants.java
@@ -4,11 +4,16 @@ package be.tritschler.fix.core;
 
 public class Constants {
 	
-	public static final char SOH = 1;
+public static final char SOH = 1;
 	
 	public static final String EQUAL = "=";
 	
 	// FIX versions
 //	public static final String PROTOCOL = "FIX";	
 //	public static final String FIX40 = "4.0";
+//	public static final String FIX41 = "4.1";
+//	// FIX groups 
+	public static final int HEADER  = 0;
+	public static final int BODY    = 1;
+	public static final int TRAILER = 2;		
 }
